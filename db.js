0eeec9cbf92e3563985e447f862a05837b876289
pranjulrathour0916 <pranjalrathore9838@gmail.com>
2025-11-13 10:55:13 +0530

    added new connection string

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const  mongoose  = require("mongoose");
 require('dotenv').config()
 
-const mongoURI = process.env.mongoURI
+const mongoURI = "mongodb+srv://pranjalrathore9838_db_user:R@th3824our@mysharebkend.0ssau6e.mongodb.net/?appName=MyShareBKend"
 
 async function connectToMongo() {
     await mongoose.connect(mongoURI)
